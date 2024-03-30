b9f2d3c910894f3bb3f2373d92ac44c5b19815d7
altaf azizi <iltfhuss@gmail.com>
2024-03-30 20:33:36 +0500

    online food delivery

--- js/resp.js
+++
@@ -1,9 +0,0 @@
-burger = document.querySelector('.burger');
-navbar = document.querySelector('.navbar');
-navList = document.querySelector('.nav-last')
-rightNav = document.querySelector('.rightNav');
-burger.addEventListener('click', () => {
-    rightNav.ckasslist.toggle('v-class-resp')
-    navList.ckasslist.toggle('v-class-resp')
-    navbar.ckasslist.toggle('h-nav-resp')
-})
